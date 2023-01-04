8b9923ed6de80207f031db2159ce366505b4048a
nickand <gonzalezn775@gmail.com>
2023-01-03 22:22:14 -0500

    ToDoCompose App Initial commit
    Adding compose, hilt and jetpack components dependencies, creating repository and dao

---
+++ app/src/main/java/com/example/to_docompose/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.to_docompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.to_docompose.ui.theme.ToDoComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoComposeTheme {
+            }
+        }
+    }
+}
