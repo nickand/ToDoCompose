1894da8b74ba2f7ab0916f5a0ceb715ea50114a6
nickand <gonzalezn775@gmail.com>
2023-01-05 23:09:47 -0500

    Adding navigation component, creating the navigation in compose

--- app/src/main/java/com/example/to_docompose/MainActivity.kt
+++ app/src/main/java/com/example/to_docompose/MainActivity.kt
@@ -10,15 +10,23 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.to_docompose.navigation.SetupNavigation
 import com.example.to_docompose.ui.theme.ToDoComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ToDoComposeTheme {
+                navController = rememberNavController()
+                SetupNavigation(navController = navController)
             }
         }
     }
